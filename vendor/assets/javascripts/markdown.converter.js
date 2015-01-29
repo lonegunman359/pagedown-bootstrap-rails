5e812c86a0a15ea1cc87393fdcd7cc6992d1d08b
lonegunman359 <lonegunman359@yahoo.de>
2015-01-29 14:13:31 +0100

    mailto link support

--- vendor/assets/javascripts/markdown.converter.js
+++ vendor/assets/javascripts/markdown.converter.js
@@ -1371,19 +1371,23 @@ else
         function encodeProblemUrlChars(url) {
             if (!url)
                 return "";
-
-            var len = url.length;
-
-            return url.replace(_problemUrlChars, function (match, offset) {
-                if (match == "~D") // escape for dollar
-                    return "%24";
-                if (match == ":") {
-                    if (offset == len - 1 || /[0-9\/]/.test(url.charAt(offset + 1)))
-                        return ":"
-                }
-                return "%" + match.charCodeAt(0).toString(16);
-            });
+            else if (/mailto:/.test(url))
+                return url;
+            else {
+                var len = url.length;
+    
+                return url.replace(_problemUrlChars, function (match, offset) {
+                    if (match == "~D") // escape for dollar
+                        return "%24";
+                    if (match == ":") {
+                        if (offset == len - 1 || /[0-9\/]/.test(url.charAt(offset + 1)))
+                            return ":"
+                    }
+                    return "%" + match.charCodeAt(0).toString(16);
+                });
+            }
         }
+        
 
 
         function escapeCharacters(text, charsToEscape, afterBackslash) {
